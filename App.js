4d7ed0bf89b7c7e533121f68e6fee95b483b0ea4
Vinayak Vishwanath Gosain <vinayakvvg@Vinayaks-Laptop.local>
2025-06-12 14:58:02 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {}, "This is React");
 
-const child = React.createElement("div", {id:'child'}, "This is child")
+const child = React.createElement("div", { id: "child" }, "This is child");
 
 const parent = React.createElement("div", { id: "parent" }, [heading, child]);
 
+const Title = () => <h1>This is a pure component</h1>;
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <h1 className="heading">Namaste React Functional Component</h1>
+      <Title />
+      {Title()}
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
