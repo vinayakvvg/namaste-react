a09171a1f2b28f5e71827987f5c9df1f8795018c
Vinayak Vishwanath Gosain <vinayakvvg@Vinayaks-Laptop.local>
2025-06-21 17:17:02 +0530

    Episode 6.2 CORS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,4 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const SWIGGY_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const CORS_PROXY = "https://proxy.cors.sh/";
