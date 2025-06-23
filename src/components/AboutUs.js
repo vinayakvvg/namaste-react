aab5acf0a77184c218bd7e029c8ad07e8b771e88
Vinayak Vishwanath Gosain <vinayakvvg@Vinayaks-Laptop.local>
2025-06-23 18:15:28 +0530

    Episode 8

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,11 @@
+import User from "./class/User";
+
 const AboutUs = () => {
   return (
     <div>
       <h1>About Us</h1>
       <h2>This is Namaste React Web Series</h2>
+      <User name={"Vinayak Gosain"} location={"Banagalore"} />
     </div>
   );
 };
