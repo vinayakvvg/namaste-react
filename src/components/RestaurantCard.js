c72bba5152fff450408b8f276c57f8da97ec2f58
VINAYAK VISHWANATH GOSAIN <gosain.vinayakvishwanath@gmail.com>
2025-06-27 14:16:12 +0530

    Episode 10
    Co-authored-by: Vinayak Vishwanath Gosain <vinayakvvg@Vinayaks-Laptop.local>

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,9 +4,9 @@ const RestaurantCard = ({ resData }) => {
   const { cuisines, avgRating, name, sla, cloudinaryImageId, costForTwo } =
     resData?.info;
   return (
-    <div className="res-card">
-      <h3>{name}</h3>
-      <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
+    <div className="p-4 m-4 w-[250px] bg-gray-100 rounded-lg hover:border border-solid border-black hover:bg-gray-200">
+      <img className="rounded-lg" src={CDN_URL + cloudinaryImageId} />
+      <h3 className="py-2 font-bold text-lg">{name}</h3>
       <h5 className="cusine">{cuisines.join(", ")}</h5>
       <h5>{avgRating} stars</h5>
       <h5>{sla.deliveryTime}</h5>
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,6 +1,6 @@
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
+    <div className="flex w-[250] h-[250] bg-gray-300 p-4 m-4">
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
